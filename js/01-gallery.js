cee436cdffbc8d94cd8af9625ec537f7271463fe
Zina <zinasaenko@hotmail.com>
2022-07-12 13:18:06 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,22 +7,50 @@ const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryCard(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryCardClick);
 
 function createGalleryCard(images) {
   return images
     .map(({ preview, original, description }) => {
       return `
-          <div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
-      <img
-        class="gallery__image"
-        src=${preview}
-        data-source=${original}
-        alt=${description}
-      />
-    </a>
-  </div>
+      <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>
+      </div>
           `;
     })
     .join('');
+}
+
+function onGalleryCardClick(event) {
+  event.preventDefault();
+  const isSrcPicture = event.target.dataset.source;
+  const isAltPicture = event.target.alt;
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${isSrcPicture}" alt="${isAltPicture}" width="800"    height="600">
+    `);
+
+  instance.show();
+
+  addEventListener('keydown', onTargetKeydown);
+
+  function onTargetKeydown(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = event.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+      instance.close();
+    }
+  }
 }
