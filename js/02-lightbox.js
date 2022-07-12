6698fa9e30f0c455b63fb7c8cccc0139d8ce9353
Zina <zinasaenko@hotmail.com>
2022-07-12 14:23:38 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const GalleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCard(galleryItems);
+GalleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCard(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>
+            `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
